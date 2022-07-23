8e62633693beeda6711ab7a61e65f2b1541f3205
Holger Mueller <9jholger@gmail.com>
2022-07-23 14:28:10 -0500

    added while loop and scoring

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -8,68 +8,92 @@ public class RockPaperScissors {
         int num;
         String userChoice = "";
         String computerChoice = "";
+        int userWins = 0;
+        int computerWins = 0;
 
-        System.out.println("Welcome to Rock, Paper, and Scissors!");
-        System.out.print("Please choose R)ock P)aper or S)cissors: ");
+        while (true) {
+            System.out.println("Welcome to Rock, Paper, and Scissors!");
+            System.out.print("Please choose R)ock P)aper or S)cissors: ");
 
-        userChoice = sc.nextLine();
+            userChoice = sc.nextLine();
 
-        num = rand.nextInt(3);
+            if (userChoice.equals("quit")) {
+                break;
+            }
 
-        // Determine computer choice
-        if (num == 0) {
-            computerChoice = "R";
-        } else if (num == 1) {
-            computerChoice = "P";
-        } else if (num == 2) {
-            computerChoice = "S";
+            num = rand.nextInt(3);
+
+            if (num == 0) {
+                computerChoice = "R";
+            } else if (num == 1) {
+                computerChoice = "P";
+            } else if (num == 2) {
+                computerChoice = "S";
+            }
+
+            if (computerChoice.equals("S")) {
+                System.out.println("The computer chose scissors.");
+            }
+
+            else if (computerChoice.equals("R")) {
+                System.out.println("The computer chose rock.");
+            }
+
+            else if (computerChoice.equals("P")) {
+                System.out.println("The computer chose paper.");
+            }
+
+            if (userChoice.equals("R") && computerChoice.equals("S")) {
+                System.out.println("The user won!");
+                userWins++;
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (userChoice.equals("P") && computerChoice.equals("R")) {
+                System.out.println("The user won!");
+                userWins++;
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (userChoice.equals("S") && computerChoice.equals("P")) {
+                System.out.println(("The user won!"));
+                userWins++;
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (userChoice.equals("S") && computerChoice.equals("R")) {
+                System.out.println("The computer won.");
+                computerWins++;
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (userChoice.equals("R") && computerChoice.equals("P")) {
+                System.out.println("The computer won.");
+                computerWins++;
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (userChoice.equals("P") && computerChoice.equals("S")) {
+                System.out.println("The computer won.");
+                computerWins++;
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (userChoice.equals((computerChoice))) {
+                System.out.println("Tie!");
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
+            else if (!userChoice.equals("R") || !userChoice.equals("P") || !userChoice.equals("S")) {
+                System.out.println("That is not an adequate choice. You forfeit. The computer wins.");
+                System.out.println("The score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+            }
+
         }
 
-        // Print computer choice
-        if (computerChoice.equals("S")) {
-            System.out.println("The computer chose scissors.");
-        }
-
-        else if (computerChoice.equals("R")) {
-            System.out.println("The computer chose rock.");
-        }
-
-        else if (computerChoice.equals("P")) {
-            System.out.println("The computer chose paper.");
-        }
-
-        // Determine winner:
-        if (userChoice.equals("R") && computerChoice.equals("S")) {
-            System.out.println("The user won!");
-        }
-
-        else if (userChoice.equals("P") && computerChoice.equals("R")) {
-            System.out.println("The user won!");
-        }
-
-        else if (userChoice.equals("S") && computerChoice.equals("P")) {
-            System.out.println(("The user won!"));
-        }
-
-        else if (userChoice.equals("S") && computerChoice.equals("R")) {
-            System.out.println("The computer won.");
-        }
-
-        else if (userChoice.equals("R") && computerChoice.equals("P")) {
-            System.out.println("The computer won.");
-        }
-
-        else if (userChoice.equals("P") && computerChoice.equals("S")) {
-            System.out.println("The computer won.");
-        }
-
-        else if (userChoice.equals((computerChoice))) {
-            System.out.println("Tie!");
-        }
-
-        else if (!userChoice.equals("R") || !userChoice.equals("P") || !userChoice.equals("S")) {
-            System.out.println("That is not an adequate choice. You forfeit. The computer wins.");
-        }
+        System.out.println("Thank you for playing!");
+        System.out.println("The final score is: User wins: " + userWins + " / " + "Computer wins: " + computerWins);
+        System.out.println("Good bye!!");
 
     }
 }
